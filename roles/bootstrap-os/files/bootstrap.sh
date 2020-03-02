f7aad55a6c7aed4d5d138959cdff54d426ec717a
Danilo Riecken P. de Morais <danilo404@icloud.com>
2020-03-02 16:36:02 +0100

    Upgrade remote python to 3.6

--- roles/bootstrap-os/files/bootstrap.sh
+++ roles/bootstrap-os/files/bootstrap.sh
@@ -11,10 +11,11 @@ if [[ -e $BINDIR/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=7.0.0
+PY_VERSION=3.6
+PYPY_VERSION=7.2.0
 
-wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-$PYPY_VERSION-linux_x86_64-portable.tar.bz2 | tar -xjf -
-mv -n pypy3.5-$PYPY_VERSION-linux_x86_64-portable pypy3
+wget -O - https://github.com/squeaky-pl/portable-pypy/releases/download/pypy${PY_VERSION}-${PYPY_VERSION}/pypy${PY_VERSION}-${PYPY_VERSION}-linux_x86_64-portable.tar.bz2 | tar -xjf -
+mv -n pypy${PY_VERSION}-${PYPY_VERSION}-linux_x86_64-portable pypy3
 
 ln -s ./pypy3/bin/pypy3 python
 $BINDIR/python --version
